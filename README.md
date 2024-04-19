cdd9a0a04ea83eea627f3e5e4f9230a9271c0af5
Carlos Quesada <cquesadad90@gmail.com>
2024-04-19 09:10:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ⚛️ Redux React Template
+
+Base template for structure of Redux in React Vite Projects.
+
+## Documentation:
+
+- [Redux Quick Start](https://redux-toolkit.js.org/tutorials/quick-start)
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+// give access to store provider globally
+import { store } from './store'
+import { Provider } from 'react-redux'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
