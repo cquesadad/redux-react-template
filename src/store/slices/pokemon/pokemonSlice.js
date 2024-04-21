683bd6c06a8c1f516e92065113556e683a633b3a
Carlos Quesada <cquesadad90@gmail.com>
2024-04-21 10:50:47 +0100

    fetch api and axios

--- src/store/slices/pokemon/pokemonSlice.js
+++ src/store/slices/pokemon/pokemonSlice.js
@@ -9,11 +9,13 @@ export const pokemonSlice = createSlice({
    },
    reducers: {
       startLoadingPokemons: (state, /* action */ ) => {
-         state.isLoading = true
+         state.isLoading = true;
       },
       setPokemons: (state, action ) => {
-        console.log(action);
-      },
+        state.isLoading = false;
+        state.page = action.payload.page;
+        state.pokemons = action.payload.pokemons;
+      }
    }
 });
 
